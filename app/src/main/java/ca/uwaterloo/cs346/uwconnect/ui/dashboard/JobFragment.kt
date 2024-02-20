0008f1910d62d679976d88d0762dc7854510c01d
Lance <lance@Lances-Mac-mini.local>
2024-02-20 17:56:22 -0500

    lance salary progress bar

--- app/src/main/java/ca/uwaterloo/cs346/uwconnect/ui/dashboard/JobFragment.kt
+++ app/src/main/java/ca/uwaterloo/cs346/uwconnect/ui/dashboard/JobFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ca.uwaterloo.cs346.uwconnect.MAX_SALARY
+import ca.uwaterloo.cs346.uwconnect.MIN_SALARY
 import ca.uwaterloo.cs346.uwconnect.databinding.FragmentJobBinding
 import com.google.android.material.chip.Chip
 
@@ -29,9 +31,16 @@ class JobFragment : Fragment() {
         }
     }
 
+
     private fun displayJobDetails(job: Job) {
+        // job title
         binding.jobTitle.text = job.title
+
+        // job description
         binding.jobDescription.text = job.description
+
+        // job details
+        // making showing as tags
         job.requirements.forEach { requirement ->
             val chip = Chip(context).apply {
                 text = requirement
@@ -39,7 +48,18 @@ class JobFragment : Fragment() {
             }
             binding.jobRequirements.addView(chip)
         }
-        binding.jobSalaryRange.text = job.salaryRange
+
+        // salary range
+        // making it a progress bar
+        val salaryRange = job.salaryRange.split(" - ").map { it.replace("[^\\d]".toRegex(), "").toInt() }
+        if (salaryRange.size == 2) {
+            val avgSalary = (salaryRange[0] + salaryRange[1]) / 2
+
+            val progressBar = binding.jobSalaryRange
+            progressBar.max = MAX_SALARY - MIN_SALARY // set up progress bar range
+            progressBar.progress = avgSalary // current progress ui set up
+
+        }
     }
 
     override fun onDestroyView() {
