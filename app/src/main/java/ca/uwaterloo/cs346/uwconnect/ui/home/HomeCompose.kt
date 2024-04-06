d076a05cfcaaf704e471d3adf70020b52f25a33e
Lance <s275xu@uwaterloo.ca>
2024-04-05 20:50:04 -0400

    lance homecompose rename

--- app/src/main/java/ca/uwaterloo/cs346/uwconnect/ui/home/HomeCompose.kt
+++ app/src/main/java/ca/uwaterloo/cs346/uwconnect/ui/home/HomeCompose.kt
@@ -22,7 +22,7 @@ import androidx.fragment.app.Fragment
 import androidx.compose.ui.platform.ComposeView
 
 
-class HomeFragmentCompose : Fragment() {
+class HomeCompose : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_compose, container, false)
