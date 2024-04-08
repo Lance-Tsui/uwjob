03c6ace4772d2a0fe272d59f401e4c02ba5067db
Ethan Roth <e2roth@uwaterloo.ca>
2024-04-07 23:36:42 -0400

    Added server project

---
+++ ktorServer/src/test/kotlin/com/cs346Jobinfo/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.cs346Jobinfo
+
+import com.cs346Jobinfo.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
