cdfb2fe3763104c162d952208e40e1bbc8694191
Lance <s275xu@uwaterloo.ca>
2024-04-08 11:42:33 -0400

    lance json string to csv format

--- app/src/main/java/ca/uwaterloo/cs346/uwconnect/data/CompanyFetch.kt
+++ app/src/main/java/ca/uwaterloo/cs346/uwconnect/data/CompanyFetch.kt
@@ -6,6 +6,7 @@ import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.runBlocking
+import org.json.JSONArray
 
 class CompanyFetch {
 
@@ -34,6 +35,28 @@ class CompanyFetch {
                 Company(parts[0].toInt(), parts[1])
             }
     }
+
+    /*
+    JSON String to csv format string
+    fun parseCompaniesJSON(jsonString: String): List<Company> {
+
+        val jsonArray = JSONArray(jsonString)
+
+        val result = StringBuilder()
+
+        for (i in 0 until jsonArray.length()) {
+            val jsonObj = jsonArray.getJSONObject(i)
+            val cId = jsonObj.getInt("c_id")
+            val cName = jsonObj.getString("c_name")
+
+            result.append("$cId,$cName\n")
+        }
+
+        println(result)
+
+        return parseCompanies(result.toString())
+    }
+    */
 
     fun fetchCompany(): List<Company> {
         return fetchRemoteFileAndParse("http://frc6399.com/conn-company.php")
